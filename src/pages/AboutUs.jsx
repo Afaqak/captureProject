2008b1bd3831655f24f8f11739830828bc331dc5
Afaqak <afaqak124>
2022-10-19 12:50:07 -0700

    second Commit

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 //page component
 import AboutSection from '../components/AboutSection'
-
+import AboutServices from '../components/AboutServices'
 const AboutUs = () => {
   return (
-    <AboutSection />
+    <>
+        <AboutSection />
+        <AboutServices />
+    </>
   )
 }
 
