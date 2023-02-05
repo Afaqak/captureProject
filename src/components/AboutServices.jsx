2dd0cc308f99a82622e7d44642716558f2e30cc7
afaqak <75029026+Afaqak@users.noreply.github.com>
2023-02-05 10:49:21 -0800

    made some changes

--- src/components/AboutServices.jsx
+++ src/components/AboutServices.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import clock from '../img/clock.svg';
 import diaphragm from '../img/diaphragm.svg';
 import money from '../img/money.svg';
@@ -6,97 +6,115 @@ import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
 import styled from 'styled-components';
 import { useInView } from 'react-intersection-observer';
-import { About, Description, Image} from "../components/styles";
+import { About, Description, Image } from '../components/styles';
 import { useAnimation } from 'framer-motion';
 import { fade } from '../animation';
 
 const AboutServices = () => {
-    const controls = useAnimation();
-    const [element, view] = useInView({ threshold: 0.3 });
-    if (view) {
-        controls.start("show");
-    }
-    else {
-        controls.start("hidden");
-    }
+  const controls = useAnimation();
+  const [element, view] = useInView({ threshold: 0.3 });
+  if (view) {
+    controls.start('show');
+  } else {
+    controls.start('hidden');
+  }
   return (
-    <Services animate={controls} variants={fade} ref={element}>
-        <Description>
-            <h2>High <span>quality</span> services.</h2>
-            <Cards className="cards">
-                <Card>
-                    <div className="icon">
-                        <img src={clock} alt="" />
-                    <h3>Efficient</h3>
-                        </div>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </Card>
-                <Card>
-                    <div className="icon">
-                        <img src={teamwork} alt="icon" />
-                    <h3>Team Work</h3>
-                        </div>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </Card>
-                <Card>
-                    <div className="icon">
-                        <img src={diaphragm} alt="icon" />
-                    <h3>Diaphragm</h3>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </Card>
-                <Card>
-                    <div className="icon">
-                        <img src={money} alt="icon" /> 
-                    <h3>Affordable</h3>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                </Card>
-                </Cards>
-            </Description>
-            <Image>
-                <img src={home2} alt="icon" />
-            </Image>
+    <Services
+      animate={controls}
+      variants={fade}
+      ref={element}
+    >
+      <Description>
+        <h2>
+          High <span>quality</span> services.
+        </h2>
+        <Cards className='cards'>
+          <Card>
+            <div className='icon'>
+              <img
+                src={clock}
+                alt=''
+              />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className='icon'>
+              <img
+                src={teamwork}
+                alt='icon'
+              />
+              <h3>Team Work</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className='icon'>
+              <img
+                src={diaphragm}
+                alt='icon'
+              />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+          <Card>
+            <div className='icon'>
+              <img
+                src={money}
+                alt='icon'
+              />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
+        <img
+          src={home2}
+          alt='icon'
+        />
+      </Image>
     </Services>
-  )
-}
+  );
+};
 
 // Styled Components
-const Services=styled(About)`
-h2{
+const Services = styled(About)`
+  h2 {
     padding-bottom: 5rem;
-}
-p{
+  }
+  p {
     width: 70%;
     padding: 2rem 0rem 4rem 0rem;
-
-}
+  }
 `;
 
-const Cards=styled.div`
-display:  grid;
-grid-template-columns: repeat(2, 1fr);
-grid-gap: 2rem;
-@media (max-width: 1300px){
+const Cards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2rem;
+  @media (max-width: 1300px) {
     grid-template-columns: 1fr;
     align-items: center;
     justify-items: center;
     width: 100%;
-}
+  }
 `;
-const Card=styled.div`
-flex-basis: 20rem;
-.icon{
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
     display: flex;
     align-items: center;
-}
+  }
 
-h3{
+  h3 {
     margin-left: 1rem;
     background: white;
     color: black;
     padding: 1rem;
-
-}
+  }
 `;
-export default AboutServices
+export default AboutServices;
--- src/components/FAQsection.jsx
+++ src/components/FAQsection.jsx
@@ -1,72 +1,62 @@
-import React from "react";
-import styled from "styled-components";
-import { About } from "./styles";
-import Toggle from "./Toggle";
-import { AnimateSharedLayout } from "framer-motion";
-import { scrollReveal } from "../animation";
-import { useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import React from 'react';
+import styled from 'styled-components';
+import { About } from './styles';
+import Toggle from './Toggle';
+import { AnimateSharedLayout } from 'framer-motion';
+import { scrollReveal } from '../animation';
+import { useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 const FAQSec = () => {
   const controls = useAnimation();
   const [element, view] = useInView({ threshold: 0.3 });
-  if(view){
-    controls.start("show");
-  }
-  else{
-    controls.start("hidden");
+  if (view) {
+    controls.start('show');
+  } else {
+    controls.start('hidden');
   }
   return (
-    <Faq variants={scrollReveal} ref={element} animate={controls} initial='hidden'>
+    <Faq
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial='hidden'
+    >
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
       <AnimateSharedLayout>
-      <Toggle  title="How Do I Start?">
-      <div className="question">
-        <div className="answer">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
-            corporis!
-          </p>
-        </div>
-        </div>
-      </Toggle>
-      <Toggle title="Daily Schedule">
-      <div className="question">
-        <div className="answer">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
-            corporis!
-          </p>
-        </div>
-      
-      </div>
-      </Toggle>
-      <Toggle title="Different Payment Methods">
-      <div className="question">
-        <div className="answer">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
-            corporis!
-          </p>
-        </div>
- 
-      </div>
-      </Toggle>
-      <Toggle title="What Products do you offer.">
-      <div className="question">
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
-            corporis!
-          </p>
-        </div>
-      </div>
-      </Toggle>
+        <Toggle title='How Do I Start?'>
+          <div className='question'>
+            <div className='answer'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!</p>
+            </div>
+          </div>
+        </Toggle>
+        <Toggle title='Daily Schedule'>
+          <div className='question'>
+            <div className='answer'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!</p>
+            </div>
+          </div>
+        </Toggle>
+        <Toggle title='Different Payment Methods'>
+          <div className='question'>
+            <div className='answer'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!</p>
+            </div>
+          </div>
+        </Toggle>
+        <Toggle title='What Products do you offer.'>
+          <div className='question'>
+            <div className='answer'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!</p>
+            </div>
+          </div>
+        </Toggle>
       </AnimateSharedLayout>
     </Faq>
   );
@@ -74,45 +64,43 @@ const FAQSec = () => {
 
 // Styled Components
 const Faq = styled(About)`
-display: block;
-span{
+  display: block;
+  span {
     display: block;
-}
+  }
 
-h2{
+  h2 {
     padding-bottom: 2rem;
     font-weight: lighter;
-}
-h4{
+  }
+  h4 {
     font-size: 2rem;
-}
-.faq-line{
+  }
+  .faq-line {
     background: #cccccc;
     height: 0.2rem;
     margin: 2rem 0rem;
     width: 100%;
-}
-.question{
+  }
+  .question {
     padding: 3rem 0rem;
     cursor: pointer;
-}
-.answer{
+  }
+  .answer {
     padding: 2rem 0rem;
-    p{
-        padding: 1rem 0rem;
+    p {
+      padding: 1rem 0rem;
     }
-}
+  }
 
-@media (max-width: 1300px){
+  @media (max-width: 1300px) {
     display: block;
     padding: 2rem 2rem;
     text-align: center;
-    span{
-        font-size: 2rem;
+    span {
+      font-size: 2rem;
     }
-
-}
-
+  }
 `;
 
 export default FAQSec;
